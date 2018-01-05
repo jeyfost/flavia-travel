19758ad34544b90df4ccdc24cc37ecd44580311d
jeyfost <foster_andrew@tut.by>
2018-01-05 09:14:27 +0300

    Создана страница входа в панель администрирования

---
+++ js/admin/index.js
@@ -0,0 +1,44 @@
+/**
+ * Created by jeyfost on 05.01.2018.
+ */
+
+document.onkeyup = function (e) {
+	e = e || window.event;
+	if (e.keyCode === 13) {
+		login();
+	}
+
+	return false;
+};
+
+function login() {
+	var login = $('#loginInput').val();
+	var password = $('#passwordInput').val();
+
+	if(login !== '') {
+		if(password !== '') {
+			$.ajax({
+				type: "POST",
+				data: {"login": login, "password": password},
+				url: "/scripts/admin/ajaxLogin.php",
+				success: function (response) {
+					switch (response) {
+						case "ok":
+							window.location.href = "admin.php";
+							break;
+						case "failed":
+							$.notify("Неверно введён логин или пароль.", "error");
+							break;
+						default:
+							$.notify(response, "warn");
+							break;
+					}
+				}
+			});
+		} else {
+			$.notify("Вы не ввели пароль", "error");
+		}
+	} else {
+		$.notify("Вы не ввели логин", "error");
+	}
+}
---
+++ scripts/admin/ajaxExit.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 05.01.2018
+ * Time: 8:06
+ */
+
+session_start();
+unset($_SESSION['userID']);
