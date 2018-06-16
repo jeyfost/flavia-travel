c3c94e42d3efdfa6795bb38016a27d336116508a
jeyfost <foster_andrew@tut.by>
2018-06-16 09:23:03 +0300

    Исправление вёрстки после правок криворуких подрядчиков со стороны

--- index.php
+++ index.php
@@ -135,7 +135,22 @@
         <div class="clear"></div>
 
         <a name="offers"></a>
-        <div class="section grey text-center">
+        <div class="section white text-center">
+    		<div class="header">
+                <br /><br />
+                <span class="headerFont">Подбор автобусных туров</span>
+                <br /><br /><br />
+            </div>
+    		<div class="container text-center" style="max-width:1200px">
+    			 <!-- BigTrip widget -->
+
+                <iframe src="https://bigtrip.by/Widget/Default/Index/1315" name="bigtrip_frame" id="bigtrip_frame" style="border: 0px none; padding: 0px; display: block; width: 100%; height: 1422px;"> Каталог автобусных туров BigTrip.by </iframe><em style="float: right;">База предоставлена каталогом <a href="https://bigtrip.by/" target="_blank">bigtrip.by</a></em><br /> <iframe src="about:black" name="bigtrip_popup" id="bigtrip_popup" style="position: fixed; border: none; left: 0; top: 0; right: 0; bottom: 0; visibility: visible; z-index: 99999; display: none; width: 100%; height: 100%; padding: 0;"></iframe><script src="https://bigtrip.by/Scripts/external/bigtrip-external-2.js?id=1315"></script>
+
+<!-- /BigTrip widget -->
+            </div>
+        </div>
+
+        <div class="section grey">
             <div class="header">
                 <br /><br /><br />
                 <span class="headerFont">Горящие предложения</span>
